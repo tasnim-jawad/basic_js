d0142cc019e2e24ecd5983f65f2fcce8ea566411
tasnim-jawad <muhammadtasnimulhasan1@gmail.com>
2022-04-21 16:07:14 +0600

    find biger number

---
+++ findBigerNumber.js
@@ -0,0 +1,13 @@
+let a = 100;
+let b = 20;
+let c = 101;
+
+if(a > b){
+    if(a > c){
+        console.log('a is bigger then all');
+    }else{
+        console.log("c is biger then all")
+    }
+}else if(b > c){
+    console.log('b is bigger then all');
+}
