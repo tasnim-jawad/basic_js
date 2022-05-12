dd30f514552eff53140fa3519b773677cb90c1c9
tasnim-jawad <muhammadtasnimulhasan1@gmail.com>
2022-05-12 12:44:52 +0600

    get data from web to dom

--- json.js
+++ json.js
@@ -2,5 +2,36 @@ fetch('https://jsonplaceholder.typicode.com/users')
   .then(response => response.json())
   .then(json => getData(json))
 
+// getData = getResource =>console.log(getResource);
 
-  getData = get => console.log(get);
+
+getData = getResource => {
+  let userName = getResource.map(names => names.username )
+  for (let i = 0; i < userName.length; i++) {
+    const element = userName[i];
+    console.log();
+    let addname = document.getElementById('userName');
+    let list = document.createElement('li');
+        list.innerHTML = '<a>' + element + '</a>';
+        addname.appendChild(list);
+  }
+}
+
+
+// fetch('https://jsonplaceholder.typicode.com/posts', {
+//   method: 'POST',
+//   body: JSON.stringify({
+//     title: 'foo',
+//     body: 'bar',
+//     userId: 1,
+//   }),
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// })
+//   .then((response) => response.json())
+//   .then(data => creatingData(data));
+
+// creatingData = data =>{
+
+// }
